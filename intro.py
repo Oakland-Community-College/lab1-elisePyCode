fdb7ab535f5f0c3f011e1802ba6873fa8dbdfa78
elisePyCode <156478420+elisePyCode@users.noreply.github.com>
2024-01-19 23:08:03 -0500

    intro

---
+++ intro.py
@@ -0,0 +1,8 @@
+print("Hello, I'm a first year student figuring out what I like to do in life.")
+print("I'm looking forward to learning how coding in python differs from the other languages")
+print("My partners intro of me: ")
+
+print(f'Partner Name: ELISE Shultz (She/Her)')
+print(f'Reason to Join Python Class: To learn new  skills as Python programming.')
+print(f'Elise prefers to watching YouTube Videos for fun when she is not in the class.')
+print(f'For fun she plays Cello .')
